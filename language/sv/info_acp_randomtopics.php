05d743a9f222108711e28ef3bd23f67d0ea5a655
Ali Faraji <alifaraji.mail@gmail.com>
2014-11-13 13:23:00 +0200

    Add swedish language files

---
+++ language/sv/info_acp_randomtopics.php
@@ -0,0 +1,32 @@
+<?php
+/**
+*
+* @package Random Topics
+* Swedish translation by Holger (http://www.maskinisten.net)
+* @copyright (c) 2014 Ali Faraji <alifaraji.mail@gmail.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+$lang = array_merge($lang, array(
+	'ACP_RANDOMTOPICS'				=> 'Slumpvalda ämnen',
+	'ACP_RANDOMTOPICS_SETTINGS'		=> 'Inställningar',
+	'ACP_RTOPICS_LIMIT'				=> 'Antal ämnen',
+	'ACP_LIMIT_ACTIVE_EXP'			=> 'Antal ämnen som skall visas',
+	'ACP_RANDOMTOPICS_TITLE'		=> 'Slumpvalda ämnen',
+	'ACP_RTOPICS_ACTIVE'			=> 'Aktivera slumpvalda ämnen?',
+	'ACP_RTOPICS_ACTIVE_EXP'		=> 'Aktivera/deaktivera tillägget Slumpvalda ämnen',
+	'ACP_RTOPICS_SETTING_SAVED'		=> 'Inställningarna har sparats.',
+	'ACP_RTOPICS_INDEX'				=> 'Visa slumpvalda ämnen på index-sidan',
+	'ACP_RTOPICS_INDEX_EXP'			=> 'Aktivera denna inställning för att visa slumpvalda ämnen på forumets index-sida',
+	'ACP_RTOPICS_VIEWTOPIC'			=> 'Visa slumpvalda ämnen på viewtopic-sidan',
+	'ACP_RTOPICS_VIEWTOPIC_EXP'		=> 'Aktivera denna inställning för att visa slumpvalda ämnen på forumets viewtopic-sida',
+));
---
+++ language/sv/randomtopics.php
@@ -0,0 +1,21 @@
+<?php
+/**
+*
+* @package Random Topics
+* Swedish translation by Holger (http://www.maskinisten.net)
+* @copyright (c) 2014 Ali Faraji <alifaraji.mail@gmail.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+$lang = array_merge($lang, array(
+	'RANDOM_TOPICS'			=> 'Slumpvalda ämnen',
+));
